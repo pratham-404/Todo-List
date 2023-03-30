ee9f155203803038da10ef9f7a143de67dcb6aec
Pratham Loya <prathamloya12345@gmail.com>
2023-03-30 22:37:19 +0530

    updated code

--- README.md
+++
@@ -1,2 +0,0 @@
-# Todo-List
-### Basic outline: [link](https://pratham-404.github.io/Todo-List/)
--- script.js
+++ script.js
@@ -2,17 +2,41 @@ let plus = document.getElementById("plus");
 let list = document.getElementById("list");
 let input = document.getElementById("input");
 
+// takes input when the plus is clicked
+plus.addEventListener("click", function () {
+
+    // console.log(document.querySelector(".done").length);
+
+    if(document.querySelector(".done") == null)
+    {
+        todo();
+        return;
+    }
+    else if(document.querySelector(".done").length!=0)
+    {
+        document.querySelector("#list").innerHTML="";
+    }
+    todo();
+});
+
+
 // takes input after pressing Enter
 input.addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.key === "Enter") {
+
+
+        if(document.querySelector(".done") == null)
+        {
+            todo();
+            return;
+        }
+        else if(document.querySelector(".done").length!=0)
+        {
+            document.querySelector("#list").innerHTML="";
+        }
         todo();
     }
-});
-
-// takes input when the plus is clicked
-plus.addEventListener("click", function () {
-    todo();
 });
 
 function todo() {
@@ -20,21 +44,27 @@ function todo() {
         // creating a <p></p>
         var para = document.createElement("li");
         // adding class for styling in para
-        para.classList.add("para-style");
+        // para.classList.add("para-style");
 
-        para.innerText = input.value;
+        para.innerText = input.value+" ";
 
-        var trash_button = document.createElement("button");
-        var trash = document.createElement("img");
-        trash.src = "static/icons/trash.svg";
-        trash_button.appendChild(trash);
+        var trash_button = document.createElement("a");
+        trash_button.innerHTML='<i class="del-btn fa-solid fa-trash"></i>';
+        // var trash = document.createElement("img");
+        // trash.src ="static/icons/trash-can.svg";
+        // trash_button.appendChild(trash);
         para.appendChild(trash_button);
-
+        var lineBreak = document.createElement("hr");
+        para.appendChild(lineBreak);
         list.appendChild(para);
         input.value = "";
 
         trash_button.addEventListener("click", function () {
             list.removeChild(para);
+            if(document.querySelectorAll("#list li").length == 0)
+            {
+                document.querySelector("#list").innerHTML="<li class='done'>Hooray, No tasks left!!</li>";
+            }
         });
     }
 }
